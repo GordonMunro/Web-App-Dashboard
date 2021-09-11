135eb4a31d314a4516cab9bad24f25a64464eb94
GordonMunro <82704461+GordonMunro@users.noreply.github.com>
2021-09-11 14:39:57 -0700

    work on charts.js

--- js/app.js
+++ js/app.js
@@ -15,41 +15,41 @@ alertBanner.addEventListener('click', e => {
 });
 // Chart Widgets
 // Traffic Chart
-const trafficCanvas = document.getElementById('traffic-chart');
-let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-        backgroundColor: 'rgba(116, 119, 191, 0.3)',
-        borderWidth: 1,
-        fill: {
-            target: 'origin',
-            above: 'rgba(99, 105, 189, 0.4)'
-          },
-          tension: .4
-    }]
-};
-let trafficOptions = {
-    aspectRatio: 2.5,
-    animation: {
-        duration: 0
-    },
-    scales: {
-        y: {
-          beginAtZero: true
-        }
-    },
-    plugins: {
-        legend: {
-          display: false
-        }
-    }
-};
-let trafficChart = new Chart(trafficCanvas, {
-    type: 'line',
-    data: trafficData,
-    options: trafficOptions
-});
+// const trafficCanvas = document.getElementById('traffic-chart');
+// let trafficData = {
+//     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+//     datasets: [{
+//         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+//         backgroundColor: 'rgba(116, 119, 191, 0.3)',
+//         borderWidth: 1,
+//         fill: {
+//             target: 'origin',
+//             above: 'rgba(99, 105, 189, 0.4)'
+//           },
+//           tension: .4
+//     }]
+// };
+// let trafficOptions = {
+//     aspectRatio: 2.5,
+//     animation: {
+//         duration: 0
+//     },
+//     scales: {
+//         y: {
+//           beginAtZero: true
+//         }
+//     },
+//     plugins: {
+//         legend: {
+//           display: false
+//         }
+//     }
+// };
+// let trafficChart = new Chart(trafficCanvas, {
+//     type: 'line',
+//     data: trafficData,
+//     options: trafficOptions
+// });
 // Bar Graph
 const dailyCanvas = document.getElementById("daily-chart");
 const dailyData = {
@@ -127,4 +127,11 @@ send.addEventListener('click', () => {
     } else {
         alert(`Message successfully sent to ${user.value}`);
     }
+});
+
+// Alerts 
+const bell = document.querySelector('.bell');
+
+bell.addEventListener('click', () => {
+    alert('You have 2 new messages. You have 2 new followers.');
 });
---
+++ js/traffic.js
@@ -0,0 +1,102 @@
+const trafficCanvas = document.getElementById('traffic-chart');
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [
+        {
+        label: 'Hourly',
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderWidth: 1,
+        fill: {
+            target: 'origin',
+            above: 'rgba(99, 105, 189, 0.4)'
+          },
+          tension: .4
+    },
+    {
+        label: 'Daily',
+        data: [950, 1350, 2000, 1000, 1800, 1350, 1550, 1850, 2050, 1300, 2400],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderWidth: 1,
+        fill: {
+            target: 'origin',
+            above: 'rgba(99, 105, 189, 0.4)'
+          },
+          tension: .4
+    },
+    {
+        label: 'Weekly',
+        data: [650, 1050, 3000, 2500, 1200, 1450, 2250, 1850, 1250, 1700, 2000],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderWidth: 1,
+        fill: {
+            target: 'origin',
+            above: 'rgba(99, 105, 189, 0.4)'
+          },
+          tension: .4
+    },
+    {
+        label: 'Monthly',
+        data: [1750, 2250, 1000, 2500, 1200, 750, 1850, 1950, 2050, 1300, 1200],
+        backgroundColor: 'rgba(116, 119, 191, 0.3)',
+        borderWidth: 1,
+        fill: {
+            target: 'origin',
+            above: 'rgba(99, 105, 189, 0.4)'
+          },
+          tension: .4
+    }
+]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+          beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+          display: false
+        }
+    }
+};
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData[i],
+    options: trafficOptions
+});
+const trafficNav = document.querySelector('.traffic-nav');
+// select button, add active class, remove class from previous button, generate new chart
+
+trafficNav.addEventListener('click', () => {
+    const liTraffic = document.querySelectorAll('.traffic-nav li');
+    for (let i = 0; i < liTraffic.length; i++) {
+        const liActive = liTraffic[i];
+        if (liActive.textContent === 'Hourly') {
+            liActive.className = 'traffic-active';
+            let trafficChart = new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficDataHourly,
+                options: trafficOptions
+            });
+        } else {
+            liActive.className = '';
+            trafficCanvas.remove();
+        }
+        if (liActive.textContent === 'Daily') {
+            liActive.className = 'traffic-active';
+            let trafficChart = new Chart(trafficCanvas, {
+                type: 'line',
+                data: trafficDataDaily,
+                options: trafficOptions
+            });
+        } else {
+            liActive.className = '';
+        }
+    }
+});
